6443dbb36f0b93acfc23bf17b8f092a3a3279e7c
ivan13711 <ecolab07@gmail.com>
2019-11-29 16:59:18 +0300

    d

--- script/form.js
+++ script/form.js
@@ -1,12 +1,32 @@
 
+const chengeModal = (elem, fun) => {                    
+elem.addEventListener('click', fun);
+}
+const UpDownForm = () => { form.classList.toggle("modal_area_top") }
+const form = document.getElementById('form');
+const cross = document.getElementById("cross");
+const us = document.getElementById("user");
+const button = document.getElementById("button");
+ chengeModal(cross,UpDownForm);
+ chengeModal(us,UpDownForm);
 
-let button = document.getElementById('submit')
-
+const inputArr = form.getElementsByTagName('input')  
+const NewObject = (arr) => {
+    let userData  = {};
+ for (let i = 0; i < arr.length; i+=1) {
+     let name = arr[i].name
+     let value= arr[i].value 
+     userData[name] = value
+ }
+return userData
+    }
+    let getUserData = NewObject(inputArr).userData
    
     
-     button.onclick = function Get_autoriz( ) {
-        let name = document.autoriz.FirstName.value;
-        let LastName = document.autoriz.LastName.value    
-        let Foto = document.autoriz.Foto.value
-        alert(name + '' +LastName)
-    }
+  //Object.assign(getUserData, NewObject(inputArr) )
+
+  
+
+
+
+
---
+++ script/userClass.js
@@ -0,0 +1,20 @@
+import  {newUserData} from './form.js'
+Class newUser {
+    constructor ( name){
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.Foto = foto;
+        t
+    }
+}
+
+//    function get  () {
+//     chengeModal(button ,function(){
+//        let a = NewObject(inputArr)
+//         alert(a.value);
+//         alert (NewObject(inputArr))
+//         return a
+//     } )
+    
+//    }
+// get ()
